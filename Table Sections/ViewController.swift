000199b5c3d36b2120734a99a6f788357d965973
Vladimir Rybalka <vladimir@Vladimirs-Mac.local>
2017-08-07 13:20:43 +0300

    Realised indexed list

--- Table Sections/ViewController.swift
+++ Table Sections/ViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    let sectionsTableIdentifier = "SectionsTableIdentifier"
+    var names: [String: [String]]!
+    var keys: [String]!
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: sectionsTableIdentifier)
+        let path = Bundle.main.path(forResource: "sortednames", ofType: "plist")
+        
+        names = NSDictionary(contentsOfFile: path!) as! [String : [String]]
+        keys = ((NSDictionary(contentsOfFile: path!))?.allKeys as! [String]).sorted(by: <)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: Table View Data Source Methods
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return keys.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let key = keys[section]
+        let nameSection = names[key]
+        
+        return (nameSection?.count)!
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return keys[section]
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: sectionsTableIdentifier, for: indexPath)
+        let key = keys[indexPath.section]
+        let nameSection = names[key]
+        
+        cell.textLabel?.text = nameSection?[indexPath.row]
+        
+        return cell
+    }
+    
+    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return keys
+    }
 
 }
 
